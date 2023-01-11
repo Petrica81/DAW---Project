571ead8036df256962cdaee3da6d27fdfc947946
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-01-12 01:31:59 +0200

    DUpa lupte seculare cu ssms

---
+++ Ziare/Models/Base/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ziare.Models.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime DateCreated { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? DateModified { get; set; }
+    }
+}
